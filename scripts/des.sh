a3429e84ea0d6dc44204925daa7686bb62ecbb50
General <rlfosterjr@gmail.com>
2015-04-24 14:26:56 -0400

    Update des.sh

--- scripts/des.sh
+++ scripts/des.sh
@@ -1,4 +1,4 @@
-$BIN/bash
+#!/bin/bash
 
 export BIN="../bin"
 export TESTS="../tests"
@@ -18,29 +18,15 @@ if [ -e $BIN/$NAM ]; then
   echo "Executing encoding tests..."
   rm $OUT/*.des
   rm $OUT/*.txt
-  { time $BIN/$NAM $IN/8.txt $OUT/8.des 1;            } 2> $OUT/time_8.txt
-  { time $BIN/$NAM $IN/64.txt $OUT/64.des 1;          } 2> $OUT/time_64.txt
-  { time $BIN/$NAM $IN/512.txt $OUT/512.des 1;        } 2> $OUT/time_512.txt
-  { time $BIN/$NAM $IN/4096.txt $OUT/4096.des 1;      } 2> $OUT/time_4096.txt
-  { time $BIN/$NAM $IN/32768.txt $OUT/32768.des 1;    } 2> $OUT/time_32768.txt
-  { time $BIN/$NAM $IN/262144.txt $OUT/262144.des 1;  } 2> $OUT/time_262144.txt
-  { time $BIN/$NAM $IN/2047152.txt $OUT/2047152.des 1;} 2> $OUT/time_2047152.txt
-
-  echo "Press D decode output files, or any key to continue"
-  read dopt
-  echo
-
-  if [ "$dopt" == "D" ]; then
-    echo "Executing decoding tests..."
-    { time $BIN/$NAM $OUT/8.des $OUT/8D.txt 0;            } 2> $OUT/timeD_8.txt
-    { time $BIN/$NAM $OUT/64.des $OUT/64D.txt 0;          } 2> $OUT/timeD_64.txt
-    { time $BIN/$NAM $OUT/512.des $OUT/512D.txt 0;        } 2> $OUT/timeD_512.txt
-    { time $BIN/$NAM $OUT/4096.des $OUT/4096D.txt 0;      } 2> $OUT/timeD_4096.txt
-    { time $BIN/$NAM $OUT/32768.des $OUT/32768D.txt 0;    } 2> $OUT/timeD_32768.txt
-    { time $BIN/$NAM $OUT/262144.des $OUT/262144D.txt 0;  } 2> $OUT/timeD_262144.txt
-    { time $BIN/$NAM $OUT/2047152.des $OUT/2047152D.txt 0;} 2> $OUT/timeD_2047152.txt
-  fi
+  { time $BIN/$NAM $IN/8.txt $OUT/8.des;            } 2> $OUT/time_8.txt
+  { time $BIN/$NAM $IN/64.txt $OUT/64.des;          } 2> $OUT/time_64.txt
+  { time $BIN/$NAM $IN/512.txt $OUT/512.des;        } 2> $OUT/time_512.txt
+  { time $BIN/$NAM $IN/4096.txt $OUT/4096.des;      } 2> $OUT/time_4096.txt
+  { time $BIN/$NAM $IN/32768.txt $OUT/32768.des;    } 2> $OUT/time_32768.txt
+  { time $BIN/$NAM $IN/262144.txt $OUT/262144.des;  } 2> $OUT/time_262144.txt
+  { time $BIN/$NAM $IN/2047152.txt $OUT/2047152.des;} 2> $OUT/time_2047152.txt
   echo "$NAM test complete"
+  cat $OUT/time_* | grep real > $OUT/des_time.txt
 else
   echo "Could not create executable $BIN/$NAM";
 fi
