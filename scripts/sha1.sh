fe60d9c7a0b387bb3e4fffdd338acef81ac74535
General <rlfosterjr@gmail.com>
2015-04-24 08:36:51 -0400

    Create sha1.sh

---
+++ scripts/sha1.sh
@@ -0,0 +1,34 @@
+$BIN/bash
+
+export BIN="../bin"
+export TESTS="../tests"
+export IN="$TESTS/in"
+export OUT="$TESTS/out/sha1"
+export NAM="tempsha1"
+
+if [ -e $BIN/$NAM ]; then
+  echo "Removing old binary..."
+  rm $BIN/$NAM;
+fi
+
+echo "Compiling $BIN/$NAM..."
+gcc -g -lrt -lcrypto ../src/$NAM.c -o $BIN/$NAM
+
+if [ -e $BIN/$NAM ]; then
+  echo "Executing encoding tests..."
+  rm $OUT/*.sha1
+  rm $OUT/*.txt
+  { time $BIN/$NAM $IN/8.txt $OUT/8.sha1 1;            } 2> $OUT/time_8.txt
+  { time $BIN/$NAM $IN/64.txt $OUT/64.sha1 1;          } 2> $OUT/time_64.txt
+  { time $BIN/$NAM $IN/512.txt $OUT/512.sha1 1;        } 2> $OUT/time_512.txt
+  { time $BIN/$NAM $IN/4096.txt $OUT/4096.sha1 1;      } 2> $OUT/time_4096.txt
+  { time $BIN/$NAM $IN/32768.txt $OUT/32768.sha1 1;    } 2> $OUT/time_32768.txt
+  { time $BIN/$NAM $IN/262144.txt $OUT/262144.sha1 1;  } 2> $OUT/time_262144.txt
+  { time $BIN/$NAM $IN/2047152.txt $OUT/2047152.sha1 1;} 2> $OUT/time_2047152.txt
+  echo "$NAM test complete"
+else
+  echo "Could not create executable $BIN/$NAM";
+fi
+
+echo "Exiting..."
+echo
