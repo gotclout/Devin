861bb38cd80ad8d3552274e8d8b8291dc7764e23
General <rlfosterjr@gmail.com>
2015-04-27 17:08:52 -0400

    Update temprsa.c

--- src/temprsa.c
+++ src/temprsa.c
@@ -3,6 +3,8 @@
 #include <openssl/rsa.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <time.h>
 
 /**
  * Decodes rf with private key and writes output to wf
@@ -10,12 +12,17 @@
  * @param const char* rf, is the read file for decoding
  * @param const char* wf, is the write file for deconding
  * @param RSA** key, is the private key for decrypting
+ * @param int ed, 0 indicates decrypt, all others indicate encrypt
  */
-int RsaDec(const char* rf, const char* wf, RSA** key)
+int RsaEncDec(const char* rf, const char* wf, RSA** key, int ed)
 {
+  struct timeval st, ft;
+  gettimeofday(&st, NULL);
+
   int r   = 0,               //return code
       v   = 0,               //decrypt value
-      len = 0;               //length read/write
+      len = 0,               //length read/write
+      rps = 0;
   unsigned char* rb;         //read buf
   unsigned char* wb;         //write buf
   FILE *i = fopen(rf, "rb"), //read file
@@ -26,17 +33,29 @@ int RsaDec(const char* rf, const char* wf, RSA** key)
     fseek(i, 0, SEEK_END);
     len = ftell(i);
     fseek(i, 0, SEEK_SET);
-    rb = (unsigned char*) malloc(256);
+    rb = (unsigned char*) malloc(len);
+    fread(rb, 1, len, i);
+    fclose(i);
     wb = (unsigned char*) malloc(256);
-    printf("RSA decrypting in file %s to out file %s\n", rf, wf);
-    while(feof(i) == 0)
+    if(ed == 0)
+      printf("RSA decrypting in file %s to out file %s\n", rf, wf);
+    else
+      printf("RSA encrypting in file %s to out file %s\n", rf, wf);
+    while(rps < len)
     {
-      memset(rb, 0, 256);
       memset(wb, 0, 256);
-      len = fread(rb, 1, 256, i);
-      v   = RSA_private_decrypt(len, rb, wb, *key, RSA_PKCS1_PADDING);
+      if(ed == 0)
+      {
+        v = RSA_private_decrypt(256, rb + rps, wb, *key, RSA_PKCS1_PADDING);
+        rps += 256;
+      }
+      else
+      {
+        v = RSA_public_encrypt(128, rb + rps, wb, *key, RSA_PKCS1_PADDING);
+        rps += 128;
+      }
       if(v != -1) fwrite(wb, 1, v, o);
-      else if(!feof(i))
+      else if(r + rps < len)
       {
         ERR_print_errors_fp(stderr);
         r = 2;
@@ -44,63 +63,13 @@ int RsaDec(const char* rf, const char* wf, RSA** key)
     }
     free(rb);
     free(wb);
-    fclose(i);
     fclose(o);
-  }
-  else
-  {
-    printf("I/O error\n");
-    if(!i && rf) printf("open %s failed\n", rf);
-    if(!o && wf) printf("open %s failed\n", wf);
-    r = 1;
-  }
-
-  return r;
-}
-
-/**
- * Encodes file rf with key and writes output to wf
- *
- * @param const char* rf, is the read file for decoding
- * @param const char* wf, is the write file for deconding
- * @param RSA** key, is the private key for decrypting
- */
-int RsaEnc(const char* rf, const char* wf, RSA** key)
-{
-  int r   = 0,               //return code
-      v   = 0,               //decrypt value
-      len = 0;               //length read/write
-  unsigned char* rb;         //read buf
-  unsigned char* wb;         //write buf
-  FILE *i = fopen(rf, "rb"), //read file
-       *o = fopen(wf, "wb"); //write file
-
-  if(i && o)
-  {
-    fseek(i, 0, SEEK_END);
-    len = ftell(i);
-    fseek(i, 0, SEEK_SET);
-    rb = (unsigned char*) malloc(256);
-    wb = (unsigned char*) malloc(256);
-    printf("RSA encrypting in file %s to out file %s\n", rf, wf);
-    while(feof(i) == 0)
-    {
-      memset(rb, 0, 256);
-      memset(wb, 0, 256);
-      len = fread(rb, 1, 128, i);
-      v = RSA_public_encrypt(len, rb, wb, *key, RSA_PKCS1_PADDING);
-      if(v != -1) fwrite(wb, 1, 256, o);
-      else if(!feof(i))
-      {
-        ERR_print_errors_fp(stderr);
-        r = 2;
-      }
-    }
-
-    free(rb);
-    free(wb);
-    fclose(i);
-    fclose(o);
+    gettimeofday(&ft, NULL);
+    double ms = ((ft.tv_sec - st.tv_sec) * 1000.0) + ((double)(ft.tv_usec - st.tv_usec)* .001);
+    FILE* tf = fopen("../doc/rsa_ms.txt", "a");
+    if(ed) fprintf(tf, "encode: %f ms\n", ms);
+    else   fprintf(tf, "decode: %f ms\n", ms);
+    fclose(tf)
   }
   else
   {
@@ -137,7 +106,7 @@ int main(int argc, char** argv)
     }
     else
     {
-      if(RsaEnc(argv[1], argv[2], &key))
+      if(RsaEncDec(argv[1], argv[2], &key, 1))
       {
         printf("RsaEnc error\n");
       }
@@ -147,7 +116,7 @@ int main(int argc, char** argv)
         memset(decout, 0, strlen(argv[2]) + 3);
         strcat(decout, argv[2]);
         strcat(decout, ".d");
-        if(RsaDec(argv[2], decout, &key))
+        if(RsaEncDec(argv[2], decout, &key, 0))
         {
           printf("RsaDec error\n");
         }
@@ -162,4 +131,3 @@ int main(int argc, char** argv)
 
   return 0;
 }
-
